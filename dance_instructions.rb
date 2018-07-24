a07419a22203b63ed8e57acb547d3b3af78f76f7
Karissa Bui <karissab002@gmail.com>
2018-07-24 16:45:40 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -3,4 +3,4 @@ puts "Step 1. Plant your legs far apart, bend knees slightly an keep posture loo
 puts "Step 2. Lift your right foot"
 puts "Step 3. Return your right foot to the ground"
 puts " Step 4. Finishing with a small skip step backwards"
-
+end
