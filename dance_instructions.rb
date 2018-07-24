881b6d98940da1e1e8b2b724faa0cd0523b7f606
Karissa Bui <karissab002@gmail.com>
2018-07-24 16:42:08 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,2 @@
-put "Step 1. Plant your legs far apart, bend knees slightly an dkeep posture loose"
+puts "Step 1. Plant your legs far apart, bend knees slightly an dkeep posture loose"
 # Code your methods below
