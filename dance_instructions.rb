e43abd0df8ec2ca8f864dcbc3a82bf84fffc6018
Karissa Bui <karissab002@gmail.com>
2018-07-24 16:48:51 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -9,4 +9,5 @@ def skip_step_left
   puts "Step 5. Lower left foot to the ground"
   puts "Step 6. Bounce left foot back up slightly, kicking it a few inches back"
   puts "Left, Right, LEft, Right"
+end
   
