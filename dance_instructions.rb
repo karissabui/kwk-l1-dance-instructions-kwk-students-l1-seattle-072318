9da7ff02f6aa8c92a9ac7ff92084cdc00dcfccbc
Karissa Bui <karissab002@gmail.com>
2018-07-24 16:45:10 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,6 @@
-puts "Step 1. Plant your legs far apart, bend knees slightly an dkeep posture loose"
+def lower_body
+puts "Step 1. Plant your legs far apart, bend knees slightly an keep posture loose"
+puts "Step 2. Lift your right foot"
+puts "Step 3. Return your right foot to the ground"
+puts " Step 4. Finishing with a small skip step backwards"
 # Code your methods below
