638ba89fe47fc19b0f7784a28e96a732270cc4b5
Karissa Bui <karissab002@gmail.com>
2018-07-24 16:48:18 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,9 +1,12 @@
 def lower_body
   puts "Step 1. Plant your legs far apart, bend knees slightly an keep posture loose"
-puts "Step 2. Lift your right foot"
-puts "Step 3. Return your right foot to the ground"
-puts " Step 4. Finishing with a small skip step backwards"
+  puts "Step 2. Lift your right foot"
+  puts "Step 3. Return your right foot to the ground"
+  puts " Step 4. Finishing with a small skip step backwards"
 end
 
 def skip_step_left
+  puts "Step 5. Lower left foot to the ground"
+  puts "Step 6. Bounce left foot back up slightly, kicking it a few inches back"
+  puts "Left, Right, LEft, Right"
   
